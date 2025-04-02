c4297c0bbe79dbfe9e049a0d99fbde2128e21be0
Adib Rahman <adibrahman44@gmail.com>
2025-04-02 13:23:12 +0600

    Update User.java

--- src/src/User.java
+++ src/src/User.java
@@ -1,11 +1,30 @@
 public class User {
-
+    private String username;
+    private String password;
     private double height; // in meters
     private double weight; // in kilograms
 
-    public User(double height, double weight) {
+    public User(String username, String password, double height, double weight) {
+        this.username = username;
+        this.password = password;
         this.height = height;
         this.weight = weight;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWeight() {
+        return weight;
     }
 
     public double calculateBMI() {
